c0193e04b338f3d2f2823631260b0b1b523fd0ca
leobuck <leobuck12@gmail.com>
2020-08-30 20:13:54 -0300

    Classe JPAUtil

---
+++ src/main/java/br/com/cursojsf/util/JPAUtil.java
@@ -0,0 +1,21 @@
+package br.com.cursojsf.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static EntityManagerFactory factory = null;
+	
+	static {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("meu-primeiro-projeto-jsf");	
+		}
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+	
+}
